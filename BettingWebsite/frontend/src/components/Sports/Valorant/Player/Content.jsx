dee01f90d7894255615abfd1244740bdf9768ebe
RahulP0529 <142198593+RahulP0529@users.noreply.github.com>
2023-08-28 20:26:56 -0500

    Fixed button

--- BettingWebsite/frontend/src/components/Sports/Valorant/Player/Content.jsx
+++ BettingWebsite/frontend/src/components/Sports/Valorant/Player/Content.jsx
@@ -7,7 +7,7 @@ const Content = () => {
     const handlePullData = async () => {
         try {
             // Make an API call to your Flask backend when the button is clicked
-            const response = await axios.get('http://localhost:5000/get_data');
+            const response = await axios.get('https://bettingproject-e2b5b449dfb7.herokuapp.com/get_data');
             console.log('Data pulled from backend:', response.data);
         } catch (error) {
             console.error('Error pulling data:', error);
